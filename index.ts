63cc6ecb1fa8c4836e70afc6aec9bdad21248364
lockieluke <sherlockluk@outlook.com>
2020-10-22 18:03:00 +0800

    Secured token

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
-const token: string = 'NzY4NzUyODU1NDE4Nzk4MTIw.X5FC4w.AMPTFxC1Wm4LHEivSsc_y3HDDuM'
-const inviteURL: string = 'https://discord.com/api/oauth2/authorize?client_id=768752855418798120&permissions=535899248&scope=bot'
-
 import {Client, Message, VoiceChannel} from 'discord.js'
 import {Formatting} from './formatting'
+import * as fs from 'fs';
+import * as path from 'path'
+
+const token: string = fs.readFileSync(path.join(__dirname, '../secret/token.txt'), {
+    encoding: 'utf8',
+});
+const inviteURL: string = 'https://discord.com/api/oauth2/authorize?client_id=768752855418798120&permissions=535899248&scope=bot';
 
 const client: Client = new Client();
 
 client.once('ready', function () {
-    console.log("Ready!")
+    console.log("Ready with token " + token)
 })
 
 client.on('message', function (message: Message) {
